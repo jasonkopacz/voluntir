1a4c85d1157d0824767b99681a9a5569afc08d65
Jason Kopacz <jtkopacz@gmail.com>
2024-06-20 16:41:24 -0400

    update firebase config

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
@@ -10,6 +10,9 @@ import { initializeApp } from "firebase/app";
 // Initialize Firebase
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  androidAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID_ANDROID,
+  iosAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID_IOS,
+  webApiKey: process.env.EXPO_PUBLIC_FIREBASE_WEB_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
